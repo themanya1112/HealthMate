71ea13626b3b596169844e0d18c0b3a93b3ec1c0
Manya Gupta <themanya1112@gmail.com>
2024-04-13 17:21:33 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,95 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:healthapp/features/user_auth/presentation/admin/admin_page.dart';
+import 'package:healthapp/features/user_auth/presentation/pages/screens/booking_page.dart';
+import 'package:healthapp/features/user_auth/presentation/pages/screens/home_page.dart';
+import 'package:healthapp/features/user_auth/presentation/pages/screens/login_page.dart';
+import 'package:healthapp/features/user_auth/presentation/pages/screens/delete.dart';
+import 'package:healthapp/features/user_auth/presentation/pages/screens/qr_gen.dart';
+import 'package:healthapp/features/user_auth/presentation/pages/screens/signup_page.dart';
+import 'package:healthapp/features/user_auth/presentation/pages/screens/appointment_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:healthapp/features/user_auth/presentation/pages/utils/config.dart';
+import 'package:healthapp/main_layout.dart';
 
-void main() {
-  runApp(const MyApp());
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
   // This widget is the root of your application.
+  static final navigatorKey = GlobalKey<NavigatorState>();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'HealthMate',
+      navigatorKey: navigatorKey, // Assigning the navigatorKey to the MaterialApp
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        //pre-define input decoration
+        inputDecorationTheme: const InputDecorationTheme(
+          focusColor: Config.primaryColor,
+          border: Config.outlinedBorder,
+          focusedBorder: Config.focusBorder,
+          errorBorder: Config.errorBorder,
+          enabledBorder: Config.outlinedBorder,
+          floatingLabelStyle: TextStyle(color: Config.primaryColor),
+          prefixIconColor: Colors.black38,
+        ),
+        scaffoldBackgroundColor: Colors.white,
+        bottomNavigationBarTheme: BottomNavigationBarThemeData(
+          backgroundColor: Config.primaryColor,
+          selectedItemColor: Colors.white,
+          showSelectedLabels: true,
+          showUnselectedLabels: false,
+          unselectedItemColor: Colors.grey.shade700,
+          elevation: 10,
+          type: BottomNavigationBarType.fixed,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      initialRoute: '/', // Initial route is SignUpPage
+      routes: {
+        '/login': (context) => LoginPage(),
+        'main':(context) => const MainLayout(),
+        '/admin': (context) => AdminPage(),
+        '/home': (context) => HomePage(),
+        '/delete': (context) => DeleteAccount(),
+        // '/booking_page': (context) => BookingPage(user: ModalRoute.of(context)!.settings.arguments as User),
+        // '/booking_page': (context) {
+        //   final args = ModalRoute.of(context)!.settings.arguments as Map<String, dynamic>;
+        //   // final user= ModalRoute.of(context)!.settings.arguments as User;
+        //   final doctorId = args['doctor_id'];
+        //   final user =args['user'];
+        //   print("User: $user");
+        //   print("Doctor ID: $doctorId");
+        //   // final doctorId = ModalRoute.of(context)!.settings.arguments as Map<String, dynamic>;
+        //   return BookingPage(user: user, doctorId: doctorId );
+        // },
+        'booking_page': (context) => BookingPage(),
+        '/': (context) => SignUpPage(), // Default route is SignUpPage
+      },
+      // onGenerateRoute: (settings) {
+      //   // If the route is a booking page route...
+      //   if (settings.name!.startsWith('/booking_page')) {
+      //     final args = settings.arguments as Map<String, dynamic>;
+      //     final user = args['user'];
+      //     final doctorId = args['doctor_id'];
+      //     // Return the BookingPage for this doctor.
+      //     return MaterialPageRoute(
+      //       builder: (context) {
+      //         return BookingPage(user: user, doctorId: doctorId);
+      //       },
+      //     );
+      //   }
+      //   // Handle other routes...
+      //   return null;
+      // },
     );
   }
 }
