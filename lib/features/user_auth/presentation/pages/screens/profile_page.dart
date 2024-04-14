8725b6916247f4861f77efae5aaa1704f5eaa63b
Manya Gupta <themanya1112@gmail.com>
2024-04-14 19:49:40 +0530

    fav page added

--- lib/features/user_auth/presentation/pages/screens/profile_page.dart
+++ lib/features/user_auth/presentation/pages/screens/profile_page.dart
@@ -6,6 +6,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 // import "../providers/dio_provider.dart";
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+import 'package:healthapp/features/user_auth/presentation/pages/screens/appointment_page.dart';
 
 class ProfilePage extends StatefulWidget {
   ProfilePage({Key? key}) : super(key: key);
@@ -35,189 +36,176 @@ class _ProfilePageState extends State<ProfilePage> {
           .get();
       // Extract the username from the user's document
       setState(() {
-        username = userData['username'] ?? 'idk'; // Replace 'username' with the actual field name in your Firestore document
+        username = userData['username'] ??
+            'idk'; // Replace 'username' with the actual field name in your Firestore document
       });
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Expanded(
-          flex: 4,
-          child: Container(
-            width: double.infinity,
-            color: Config.primaryColor,
-            child: Column(
-              children: <Widget>[
-                SizedBox(
-                  height: 110,
-                ),
-                CircleAvatar(
-                  radius: 65.0,
-                  // backgroundImage: AssetImage('assets/profile1.jpg'),
-                  backgroundColor: Colors.white,
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Text(
-                  'manya',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              color: Config.primaryColor,
+              child: Column(
+                children: <Widget>[
+                  SizedBox(
+                    height: 110,
                   ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Text(
-                  '23 Years Old | Female',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 15,
+                  CircleAvatar(
+                    radius: 65.0,
+                    backgroundColor: Colors.white,
                   ),
-                ),
-              ],
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    'manya',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    '23 Years Old | Female',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 15,
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ),
-        Expanded(
-          flex: 5,
-          child: Container(
-            color: Colors.grey[200],
-            child: Center(
-              child: Card(
-                margin: const EdgeInsets.fromLTRB(0, 45, 0, 0),
-                child: Container(
-                  width: 300,
-                  height: 250,
-                  child: Padding(
-                    padding: const EdgeInsets.all(10),
-                    child: Column(
-                      children: [
-                        const Text(
-                          'Profile',
-                          style: TextStyle(
-                            fontSize: 17,
-                            fontWeight: FontWeight.w800,
-                          ),
-                        ),
-                        Divider(
-                          color: Colors.grey[300],
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
+            Container(
+              color: Colors.grey[200],
+              child: Center(
+                child: Card(
+                  margin: const EdgeInsets.fromLTRB(0, 45, 0, 0),
+                  child: Container(
+                    width: 300,
+                    height: 250,
+                    child: SingleChildScrollView( // Wrap the Column with SingleChildScrollView
+                      child: Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: Column(
                           children: [
-                            Icon(
-                              Icons.person,
-                              color: Colors.blueAccent[400],
-                              size: 35,
-                            ),
-                            const SizedBox(
-                              width: 20,
-                            ),
-                            TextButton(
-                              onPressed: () {},
-                              child: const Text(
-                                "Profile",
-                                style: TextStyle(
-                                  color: Config.primaryColor,
-                                  fontSize: 15,
-                                ),
+                            const Text(
+                              'Profile',
+                              style: TextStyle(
+                                fontSize: 17,
+                                fontWeight: FontWeight.w800,
                               ),
+                            ),
+                            Divider(
+                              color: Colors.grey[300],
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Icon(
+                                  Icons.person,
+                                  color: Colors.blueAccent[400],
+                                  size: 35,
+                                ),
+                                const SizedBox(
+                                  width: 20,
+                                ),
+                                TextButton(
+                                  onPressed: () {},
+                                  child: const Text(
+                                    "Profile",
+                                    style: TextStyle(
+                                      color: Config.primaryColor,
+                                      fontSize: 15,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Config.spaceSmall,
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Icon(
+                                  Icons.history,
+                                  color: Colors.yellowAccent[400],
+                                  size: 35,
+                                ),
+                                const SizedBox(
+                                  width: 20,
+                                ),
+                                TextButton(
+                                  onPressed: () {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(builder: (context) => AppointmentPage()),
+                                    );
+                                  },
+                                  child: const Text(
+                                    "History",
+                                    style: TextStyle(
+                                      color: Config.primaryColor,
+                                      fontSize: 15,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Config.spaceSmall,
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Icon(
+                                  Icons.logout_outlined,
+                                  color: Colors.lightGreen[400],
+                                  size: 35,
+                                ),
+                                const SizedBox(
+                                  width: 20,
+                                ),
+                                TextButton(
+                                  onPressed: () async {
+                                    try {
+                                      // Sign out the user from Firebase Authentication
+                                      await FirebaseAuth.instance.signOut();
+
+                                      // Redirect to the login page or any other page as needed
+                                      Navigator.of(context).pushReplacementNamed(
+                                          '/login'); // Replace '/login' with your actual login page route
+                                    } catch (e) {
+                                      // Handle sign out errors
+                                      print('Sign out error: $e');
+                                    }
+                                  },
+                                  child: const Text(
+                                    "Logout",
+                                    style: TextStyle(
+                                      color: Config.primaryColor,
+                                      fontSize: 15,
+                                    ),
+                                  ),
+                                ),
+                              ],
                             ),
                           ],
                         ),
-                        Config.spaceSmall,
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            Icon(
-                              Icons.history,
-                              color: Colors.yellowAccent[400],
-                              size: 35,
-                            ),
-                            const SizedBox(
-                              width: 20,
-                            ),
-                            TextButton(
-                              onPressed: () {},
-                              child: const Text(
-                                "History",
-                                style: TextStyle(
-                                  color: Config.primaryColor,
-                                  fontSize: 15,
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                        Config.spaceSmall,
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            Icon(
-                              Icons.logout_outlined,
-                              color: Colors.lightGreen[400],
-                              size: 35,
-                            ),
-                            const SizedBox(
-                              width: 20,
-                            ),
-                            TextButton(
-                              onPressed: () async {
-                                try {
-                                  // Sign out the user from Firebase Authentication
-                                  await FirebaseAuth.instance.signOut();
-
-                                  // Redirect to the login page or any other page as needed
-                                  Navigator.of(context).pushReplacementNamed('/login'); // Replace '/login' with your actual login page route
-                                } catch (e) {
-                                  // Handle sign out errors
-                                  print('Sign out error: $e');
-                                }
-                                // final SharedPreferences prefs =
-                                // await SharedPreferences.getInstance();
-                                // final token = prefs.getString('token') ?? '';
-
-                                // if (token.isNotEmpty && token != '') {
-                                  //logout here
-                                  // final response =
-                                  // await DioProvider().logout(token);
-
-                                  // if (response == 200) {
-                                    //if successfully delete access token
-                                    //then delete token saved at Shared Preference as well
-                                    // await prefs.remove('token');
-                                    // setState(() {
-                                      //redirect to login page
-                                      // MyApp.navigatorKey.currentState!
-                                      //     .pushReplacementNamed('/');
-                                    // });
-                                  // });
-                                // }
-                              },
-                              child: const Text(
-                                "Logout",
-                                style: TextStyle(
-                                  color: Config.primaryColor,
-                                  fontSize: 15,
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
+                      ),
                     ),
                   ),
                 ),
+
               ),
             ),
-          ),
+          ],
         ),
-      ],
+      ),
     );
   }
 }
